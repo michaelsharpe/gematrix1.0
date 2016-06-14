269639ac380e0f18a4e6c49808959c0e0b13b488
Michael Sharpe <mike.s@myplanet.com>
2016-06-14 11:50:53 -0400

    Parse all types of numeral entries from beginning of paragraph.

--- scraper_helper.js
+++ scraper_helper.js
@@ -1,3 +1,6 @@
+/* eslint no-unused-vars: 0 */
+/* eslint prefer-const: 0 */
+
 const s = require('underscore.string');
 
 // Regexs
@@ -76,32 +79,123 @@ function parseNumeral(line, cb) {
 
 function parseWordEntry(sentence, cb) {
   const result = {};
+  // Clean the sentences. Remove '+'.
+  const strippedSentence = sentence.split('').filter(l => l !== '+').join('');
+  const cleanedSentence = s(strippedSentence).clean().value();
   // First we get an array of all the words
-  const arr = sentence.split(' ');
+  const arr = cleanedSentence.split(' ');
   const singleHebrewRegex = new RegExp(/^(Ch|Sh|Th|Tz|\[Heh\]|Aleph|[ABGDHVZCTIKLMNSOPQR])$/);
+  const hebrewWordRegex = new RegExp(/^(Ch|Sh|Th|Tz|[ABGDHVZCTIKLMNSOPQR-])*$/);
 
-  arr.forEach((word, index, wordArray) => {
-    // check the first word to see if it is a match of a single hebrew letter
-    if (index === 0) {
-      // Filter out single letters without pronunciation, and longer entries.    
-        const letterMatch = word.match(singleHebrewRegex);
-        if (letterMatch && letterMatch[0] !== '') {
-          if (wordArray.length > 1 && wordArray.length < 3) {
-            result.word = wordArray[0];
-            result.pronunciation = wordArray[1];
-          }
-        }
+  // SINGLE HEBREW LETTER
+  // check the first word to see if it is a match of a single hebrew letter
+  const letterMatch = arr[0].match(singleHebrewRegex);
 
-      // Next, we should
+  if (letterMatch && letterMatch[0] !== '') {
+    if (arr.length === 2) {
+      result.word = arr[0];
+      result.pronunciation = arr[1];
+      result.langauge = 'hebrew';
+      result.comment = '';
+
+      return result;
     }
-  });
+  }
+
+  // HEBREW WORD
+  const hebrewWordMatch = arr[0].match(hebrewWordRegex);
+  const hebrewWords = [];
+  const otherWords = [];
+
+  if (hebrewWordMatch && hebrewWordMatch[0] !== '' && arr[0].length !== 1) {
+    result.language = 'hebrew';
+
+    arr.forEach(word => {
+      const matchWord = word.match(hebrewWordRegex);
+
+      if (matchWord && matchWord[0] !== '' && otherWords.length === 0) {
+        hebrewWords.push(word);
+      } else {
+        otherWords.push(word);
+      }
+    });
+
+    result.word = hebrewWords.join(' ');
+
+    if (otherWords.length > hebrewWords.length + 2) {
+      result.comment = otherWords.join(' ');
+    } else {
+      result.pronunciation = otherWords.join(' ');
+    }
+
+    return result;
+  }
+
+  // GREEK
+  const greekWordRegex = new RegExp(/([µζχξωβνμασδφγηςκλθωερτψυιοπάέήίϊΐόύϋΰώ*?])*/);
+  const greekLetterRegex = new RegExp(/([µζχξωβνμασδφγηςκλθωερτψυιοπάέήίϊΐόύϋΰώ])/);
+  const greekEnglishRegex = new RegExp(/([a-zA-Z\s]*\s*\(Gr\){1})/);
+
+  const greekWordMatch = arr[0].match(greekWordRegex);
+  if (greekWordMatch && greekWordMatch[0] !== '') {
+    result.language = 'greek';
+    // First we need to strip out this broken greek
+    const greekStripped = sentence.split('').filter(l => {
+      const englishMatch = l.match(new RegExp(/[a-zA-Z\s\(\)]/));
+      // console.log(englishMatch);
+      if (englishMatch) {
+        return l;
+      }
+    }).join('');
+
+    const cleanGreekSentence = s(greekStripped).clean().value();
+
+    if (cleanGreekSentence !== '') {
+      result.word = cleanGreekSentence;
+      return result;
+    }
+  } else {
+    // This ensures that we filter out words that already have greek letters
+    // English greek entry
+
+    const greekEnglishMatch = arr.join('').match(greekEnglishRegex);
+
+    if (greekEnglishMatch && greekEnglishMatch[0] !== '') {
+      result.word = cleanedSentence;
+      result.language = 'greek';
+      return result;
+    }
+  }
+
+  // Latin
+  const latinRegex = new RegExp(/([a-zA-Z\s,]*\s*\(Lt.?\){1})(\s[A-Za-z]*)?/);
+
+  const latinMatch = arr.join(' ').match(latinRegex);
+  if (latinMatch && latinMatch[0] !== '') {
+    result.language = 'latin';
+
+    if (latinMatch[1]) {
+      result.word = latinMatch[1];
+    }
+
+    if (latinMatch[2]) {
+      result.translation = latinMatch[2];
+    }
+
+    return result;
+  }
 }
 
 function parseParagraph(paragraph, cb) {
   // First we convert the string into a clean array of sentences.
   const arr = s(paragraph).clean().split('.');
   // Next we check to see if the first index is a word entry
-  parseWordEntry(arr[0]);
+  const entry = parseWordEntry(arr[0]);
+
+  if (entry) {
+    const {word, pronunciation, language, translation} = entry;
+    console.log(entry);
+  }
 }
 
 function isEmpty(obj) {
